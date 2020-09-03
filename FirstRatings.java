d9c006c41a2c8cc5e02202fc9dc53b3f1e2bbea6
sanjac0002 <sanjac0002@gmail.com>
2020-09-03 08:24:58 +0530

    third commit

---
+++ FirstRatings.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of FirstRatings here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+import java.util.*;
+import org.apache.commons.csv.*;
+public class FirstRatings 
+{
+    public ArrayList loadMovies(String filename) 
+    {
+        FileResource fr= new FileResources;
+        
+    }
+
+}
